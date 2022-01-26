e7cc127c0d195f57eaa9eb2ddb9204621cf3cb6e
Stecathw <77147042+Stecathw@users.noreply.github.com>
2022-01-26 10:31:38 +0100

    description of code

--- podologie/slots.py
+++ podologie/slots.py
@@ -3,7 +3,50 @@ from datetime import datetime, date, time
 
 def get_slots(hours, appointments, pause, duration):
     """
-    Return a list of available slots
+    Entries :    
+        hours : 
+            Opening hours of the day.
+            Tuples of datetime -> (starting date+time, ending date+time)
+            (datetime.datetime(2022, 1, 27, 9, 0), datetime.datetime(2022, 1, 27, 17, 0))
+            
+        appointments : 
+            existing appointments
+            List of datetimes tuples
+            [(datetime.datetime(2022, 1, 27, 9, 0), datetime.datetime(2022, 1, 27, 10, 0))]
+        
+        pause : 
+            Mid day pause 
+            List of datetime tuples 
+            [(datetime.datetime(2022, 1, 27, 12, 0), datetime.datetime(2022, 1, 27, 13, 0))]
+            
+        duration : 
+            duration of the choosen service
+            eg: 0:30:00
+        
+    From entries of tuples, it creates a new TAKEN slot list of datetime tuples 
+        slots = [(datetime.datetime(2022, 1, 27, 9, 0), datetime.datetime(2022, 1, 27, 9, 0)), 
+                (datetime.datetime(2022, 1, 27, 9, 0), datetime.datetime(2022, 1, 27, 10, 0)), 
+                (datetime.datetime(2022, 1, 27, 10, 0), datetime.datetime(2022, 1, 27, 10, 30)), 
+                (datetime.datetime(2022, 1, 27, 10, 30), datetime.datetime(2022, 1, 27, 11, 0)), 
+                (datetime.datetime(2022, 1, 27, 11, 0), datetime.datetime(2022, 1, 27, 11, 30)), 
+                (datetime.datetime(2022, 1, 27, 12, 0), datetime.datetime(2022, 1, 27, 13, 0)), 
+                (datetime.datetime(2022, 1, 27, 17, 0), datetime.datetime(2022, 1, 27, 17, 0))]
+        These slots are unavailable.
+    
+    By looping through the unvailable slots it calculate starting and ending time of available slots in the day
+        start               end
+        2022-01-27 09:00:00 2022-01-27 09:00:00
+        2022-01-27 10:00:00 2022-01-27 10:00:00
+        2022-01-27 10:30:00 2022-01-27 10:30:00
+        2022-01-27 11:00:00 2022-01-27 11:00:00
+        2022-01-27 11:30:00 2022-01-27 12:00:00
+        2022-01-27 13:00:00 2022-01-27 17:00:00
+        
+    And while start hour + duration of service <= end hour
+        2022-01-27 11:30:00 + 0:30:00 = 2022-01-27 12:00:00
+        it generates an available slot, and append the new tuple to available list : [(2022-01-27 11:30:00, 2022-01-27 12:00:00)...]
+        
+    Returns a list of available slots
     """
     available = []
     slots = sorted([(hours[0], hours[0])] + appointments + pause + [(hours[1], hours[1])])
